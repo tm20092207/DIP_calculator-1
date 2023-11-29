d4f07ab39dddbc454b2e71f6967f0b8fa9d51c20
nuengnueng <hw45498@horwang.ac.th>
2023-11-29 11:10:07 +0700

    oo

--- mycalculator.py
+++ mycalculator.py
@@ -3,16 +3,15 @@ import tkinter as tk
 class mycalculator:
     def __init__(self):
 
-        self.root = tk.Tk()
+      self.root = tk.Tk()
 
-        self.root.geometry ("300x300")
-        self.root.title("mycalculator")
+      self.root.geometry ("300x300")
+      self.root.title("mycalculator")
 
-        self.label = tk.Label(self.root, text="hello world!", font=('Arial', 18)) 
-        self.label.pack()
-        self.button = tk.Button(self.root, text="Click Here!", height=4)
-        self.button.place(x=20, y=50)
-
-        self.root.mainloop()
+      self.label = tk.Label(self.root, text="hello world!", font=('Arial', 18)) 
+      self.label.pack()
+      self.button = tk.Button(self.root, text="Click Here!", height=4)
+      self.button.place(x=20, y=50)
+      self.root.mainloop()
 
 mycalculator()
