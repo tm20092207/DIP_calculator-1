414fdf16340d51b35b46949a1e1d9b03d5ac4799
nuengnueng <hw45498@horwang.ac.th>
2023-11-29 10:58:27 +0700

    h

---
+++ mycalculator.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+class mycalculator:
+    def __init__(self):
+
+        self.root = tk.Tk()
+
+        self.root.geometry ("300x300")
+        self.root.title("mycalculator")
+
+        self.label = tk.Label(self.root, text="hello world!", font=('Arial', 18)) 
+        self.label.pack()
+        self.button = tk.Button(self.root, text="Click Here!", height=4)
+        self.button.place(x=20, у=50)
+
+        self.root.mainloop()
+
+mycalculator()
