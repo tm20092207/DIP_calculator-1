e3cb8b6e2b682a4f0cb72e449cf7b4aa42ae3c9c
nuengnueng <hw45498@horwang.ac.th>
2023-11-29 11:27:01 +0700

    development

--- main.py
+++ main.py
@@ -10,8 +10,29 @@ class mycalculator:
 
       self.label = tk.Label(self.root, text="hello world!", font=('Arial', 18)) 
       self.label.pack()
-      self.button = tk.Button(self.root, text="Click Here!", height=4)
+
+      self.button = tk.Button(self.root, text="1", height=4)
+      self.button.place(x=0, y=0)
+      self.button = tk.Button(self.root, text="2", height=4)
+      self.button.place(x=20, y=0)
+      self.button = tk.Button(self.root, text="3", height=4)
+      self.button.place(x=40, y=0)
+      self.button = tk.Button(self.root, text="4", height=4)
       self.button.place(x=20, y=50)
+      self.button = tk.Button(self.root, text="5", height=4)
+      self.button.place(x=20, y=50)
+      self.button = tk.Button(self.root, text="6", height=4)
+      self.button.place(x=20, y=50)
+      self.button = tk.Button(self.root, text="7", height=4)
+      self.button.place(x=20, y=50)
+      self.button = tk.Button(self.root, text="8", height=4)
+      self.button.place(x=20, y=50)
+      self.button = tk.Button(self.root, text="9", height=4)
+      self.button.place(x=20, y=50)
+
+
+
+
       self.root.mainloop()
 
 mycalculator()
